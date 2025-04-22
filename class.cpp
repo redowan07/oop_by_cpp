33edad9c354f191e35cdffcb7ae93356d16e6ecb
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-04-22 22:14:25 +0600

    new

---
+++ class.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Student{    //student is a new datatype
+public:
+    string name;
+    int roll;
+    float cgpa;
+    
+};
+int main(){
+    Student p1;
+    p1.name="Redowan Bhuiyan";
+    p1.roll=07;
+    p1.cgpa = 3.84;
+    
+    
+    Student p2;
+    p2.name="Saif-ur-Rahman";
+    p2.roll=43;
+    p2.cgpa=3.57;
+
+    cout<<p1.name<<"  roll: "<<p1.roll<<"  CGPA: "<<p1.cgpa<<endl;
+    cout<<p2.name<<"  roll: "<<p2.roll<<"  CGPA: "<<p2.cgpa<<endl;
+
+}
