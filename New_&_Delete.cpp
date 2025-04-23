dc0b2d8d1a00dad7ac853c13af4e6c8a52be7c0a
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-04-23 23:30:53 +0600

    new

---
+++ New_&_Delete.cpp
@@ -0,0 +1,20 @@
+////using new and delete
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//    int *p;
+//    p=new int;  //allocates room for an integer
+//    if(!p){
+//        cout<<"allocation error\n";
+//        return 1;
+//    }
+//    *p=100;
+//    cout<<"here is integer at p: "<<*p<<"\n";
+//    delete p;  //release memory
+//    return 0;
+//}
+
+
+
---
+++ constructor_04.cpp
@@ -0,0 +1,18 @@
+//
+//#include<iostream>
+//using namespace std;
+//
+//
+//class Student{
+//public:
+//    string name;
+//    int roll;
+//    float gpa;
+//    Student(){  //default constructor
+//        
+//    };
+//    Student(string s,int r){
+//        name =s;
+//        roll=
+//    }
+//};
