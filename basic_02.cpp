c87e3a7a85aa1fda449cff6f3e733ab5f01769af
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-04-23 11:57:31 +0600

    new

--- basic_02.cpp
+++ basic_02.cpp
@@ -1,55 +1,55 @@
-//here i have checked whether its passing by value or reference
-
-#include<iostream>
-using namespace std;
-class Car{
-public:
-    string name;
-    int price;
-    int seats;
-    string type;
-    
-};
-void print(Car c){
-    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
-}
-//lets find if its passing by reference or value
-void change(Car c){    //i have checked it's only passes the value
-    c.name="changed by values";
-    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
-    cout<<endl;
-}
-
-void changeReference(Car& c){  //i have checked & its passes by reference
-    c.name="Changed by Reference";
-    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
-    cout<<endl;
-}
-
-int main(){
-    Car c1;
-    c1.name="toyota";
-    c1.price=900000;
-    c1.seats=5;
-    c1.type="private car";
-    
-    Car c2;
-    c2.name="Mercedes";
-    c2.price=90000000;
-    c2.seats=5;
-    c2.type="Jeep";
-    
-    Car c3;
-    c3.name="Range Rover";
-    c3.price=15000000;
-    c3.seats=7;
-    c3.type="SUV";
-    
-    change(c1);
-    
-    changeReference(c2);
-    
-    print(c1);
-    print(c2);
-    print(c3);
-}
+////here i have checked whether its passing by value or reference
+//
+//#include<iostream>
+//using namespace std;
+//class Car{
+//public:
+//    string name;
+//    int price;
+//    int seats;
+//    string type;
+//    
+//};
+//void print(Car c){
+//    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
+//}
+////lets find if its passing by reference or value
+//void change(Car c){    //i have checked it's only passes the value
+//    c.name="changed by values";
+//    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
+//    cout<<endl;
+//}
+//
+//void changeReference(Car& c){  //i have checked & its passes by reference
+//    c.name="Changed by Reference";
+//    cout<<"name: "<<c.name<<" || "<<"price: "<<c.price<<" || "<<"seats: "<<c.seats<<" || "<<"type: "<<c.type<<endl;
+//    cout<<endl;
+//}
+//
+//int main(){
+//    Car c1;
+//    c1.name="toyota";
+//    c1.price=900000;
+//    c1.seats=5;
+//    c1.type="private car";
+//    
+//    Car c2;
+//    c2.name="Mercedes";
+//    c2.price=90000000;
+//    c2.seats=5;
+//    c2.type="Jeep";
+//    
+//    Car c3;
+//    c3.name="Range Rover";
+//    c3.price=15000000;
+//    c3.seats=7;
+//    c3.type="SUV";
+//    
+//    change(c1);
+//    
+//    changeReference(c2);
+//    
+//    print(c1);
+//    print(c2);
+//    print(c3);
+//}
--- constructor.cpp
+++ constructor.cpp
@@ -1,29 +1,29 @@
-#include<iostream>
-using namespace std;
-class Student{    //student is a new datatype
-public:
-    string name;
-    int roll;
-    float cgpa;
-    Student(string n,int r,float gp){     //constructor
-        name=n;
-        roll=r;
-        cgpa=gp;
-    }
-};
-void print(Student s){
-    cout<<s.name<<" || "<<"roll: "<<s.roll<<" || "<<"cgpa: "<<s.cgpa<<endl;
-}
-int main(){
-    Student s1("Redowan",07,3.84);
-
-    Student s2("Saifur-Rahman",43,3.54);
-    
-    Student s3("Fayaz-Ali-Muktadir",2,3.90);
-    
-    print(s1);
-    print(s2);
-    print(s3);
-    
-}
-
+//#include<iostream>
+//using namespace std;
+//class Student{    //student is a new datatype
+//public:
+//    string name;
+//    int roll;
+//    float cgpa;
+//    Student(string n,int r,float gp){     //constructor
+//        name=n;
+//        roll=r;
+//        cgpa=gp;
+//    }
+//};
+//void print(Student s){
+//    cout<<s.name<<" || "<<"roll: "<<s.roll<<" || "<<"cgpa: "<<s.cgpa<<endl;
+//}
+//int main(){
+//    Student s1("Redowan",07,3.84);
+//
+//    Student s2("Saifur-Rahman",43,3.54);
+//    
+//    Student s3("Fayaz-Ali-Muktadir",2,3.90);
+//    
+//    print(s1);
+//    print(s2);
+//    print(s3);
+//    
+//}
+//
