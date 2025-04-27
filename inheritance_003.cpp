af6d560809f9bb1f869d9daf4781d8ba70c5ca85
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-04-27 15:32:09 +0600

    new

---
+++ inheritance_003.cpp
@@ -0,0 +1,35 @@
+////access specifier (protected)
+//
+//#include<iostream>
+//using namespace std;
+//
+////base class
+//class Employee{
+//    //protected access specifier
+//protected:
+//    int salary;
+//};
+//
+////derived class
+//class programmer: public Employee{
+//public:
+//    int bonus;
+//    
+//    void setSalary(int x){
+//        salary=x;
+//    }
+//    
+//    int getSalary(){
+//        return salary;
+//    }
+//};
+//
+//int main(){
+//    programmer obj;
+//    obj.setSalary(50000);
+//    obj.bonus=1500;
+//    cout<<"Salary : "<<obj.getSalary()<<endl;  //salary is a protected member of Employee class, so we can't direct access through obj.salary
+//    cout<<"Bonus : "<<obj.bonus<<endl;
+//    return 0;
+//    
+//}
