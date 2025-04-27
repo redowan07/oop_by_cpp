cedeabaf30171928fc651cc27f264ad69363cc5b
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-04-27 11:45:50 +0600

    new

---
+++ inheritance_002.cpp
@@ -0,0 +1,38 @@
+////multiple inheritence
+////A class can be derived from more than one base class,using a comma-separated list
+//
+//
+//#include<iostream>
+//using namespace std;
+//
+//
+////base class
+//class Myclass{
+//public:
+//    void myFunction(){
+//        cout<<"Tor mayre bap eida amar class"<<endl<<endl;
+//    }
+//};
+//
+////another base class
+//class OtherClass{
+//public:
+//    void karFunction(){
+//        cout<<"Eida abar kar class re"<<endl<<endl;
+//    }
+//};
+//
+////derived class
+//class ChildClass:public Myclass,public OtherClass{
+//
+//};
+//
+//int main(){
+//    
+//    ChildClass obj;
+//    
+//    obj.myFunction();    //accessed attributes from Myclass
+//    obj.karFunction();     //accessed attributes from OtherClass
+//    return 0;
+//}
+//
